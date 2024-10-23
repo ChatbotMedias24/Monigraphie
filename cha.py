d6aa804af17c627f44c4f4f74d6ef75b4994afff
Chatbot <134295092+ChatbotMedias24@users.noreply.github.com>
2024-10-23 18:56:11 +0100

    Update cha.py

--- cha.py
+++ cha.py
@@ -1,15 +1,12 @@
 import streamlit as st
+import openai
+import streamlit as st
 from dotenv import load_dotenv
 import pickle
-from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
-from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.prompts import (
@@ -22,9 +19,18 @@ from streamlit_chat import message  # Importez la fonction message
 import toml
 import docx2txt
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
-from langchain.callbacks.base import BaseCallbackHandler
+import docx2txt
+from dotenv import load_dotenv
 if 'previous_question' not in st.session_state:
     st.session_state.previous_question = []
+
+# Chargement de l'API Key depuis les variables d'environnement
+load_dotenv(st.secrets["OPENAI_API_KEY"])
+
+# Configuration de l'historique de la conversation
+if 'previous_questions' not in st.session_state:
+    st.session_state.previous_questions = []
+
 st.markdown(
     """
     <style>
@@ -75,18 +81,32 @@ st.markdown(
         .message-container.assistant {
             justify-content: flex-start; /* Aligner à droite pour l'assistant */
         }
+        input[type="text"] {
+            background-color: #E0E0E0;
+        }
+
+        /* Style for placeholder text with bold font */
+        input::placeholder {
+            color: #555555; /* Gris foncé */
+            font-weight: bold; /* Mettre en gras */
+        }
+
+        /* Ajouter de l'espace en blanc sous le champ de saisie */
+        .input-space {
+            height: 20px;
+            background-color: white;
+        }
+    
     </style>
     """,
     unsafe_allow_html=True
 )
-
 # Sidebar contents
 textcontainer = st.container()
 with textcontainer:
     logo_path = "medi.png"
-    logoo_path = "mono.png"
+    logoo_path = "NOTEPRESENTATION.png"
     st.sidebar.image(logo_path,width=150)
-    st.sidebar.image(logoo_path,width=150)
    
     
 st.sidebar.subheader("Suggestions:")
@@ -95,87 +115,98 @@ questions = [
         "Qu'est-ce qu'un datacenter ?",
         "Comment le gouvernement marocaine soutient-il les projets de transformation digitale dans le pays ?",
         "Quelles sont les prévisions de croissance du marché de datacenters au Maroc jusqu'en 2026 ?",
-        "Quels défis le Maroc doit-il relever pour devenir un hub technologique africain dans le domaine des Datacenters ?"
-    ]    
- 
+        "Quels défis le Maroc doit-il relever pour devenir un hub technologique africain dans le domaine des Datacenters ?"  ]    
 load_dotenv(st.secrets["OPENAI_API_KEY"])
-conversation_history = StreamlitChatMessageHistory()
-
+# Initialisation de l'historique de la conversation dans `st.session_state`
+if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = StreamlitChatMessageHistory()
 def main():
     conversation_history = StreamlitChatMessageHistory()  # Créez l'instance pour l'historique
-    st.header("Rapport Monographie sectorielle : Les Datacenters  💬")
-    # upload a PDF file
-    docx = 'monographie.docx'
- 
-    # st.write(pdf)
-    if docx is not None:
-        text = docx2txt.process(docx)
-         # Get the first page as an image
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-            )
-        chunks = text_splitter.split_text(text=text)
- 
-        # # embeddings
-        # st.write(chunks)
- 
-        embeddings = OpenAIEmbeddings()
-        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        with open("aaa.pkl", "wb") as f:
-            pickle.dump(VectorStore, f)
- 
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        #st.markdown("**Posez vos questions ci-dessous:**")
-        st.markdown('<p style="margin-bottom: 0;"><h7><b>Posez vos questions ci-dessous:</b></h7></p>', unsafe_allow_html=True)
 
-        query_input = st.text_input("")
-        selected_questions = st.sidebar.radio("****Choisir :****",questions)
+    st.header("Rapport Monographie sectorielle : Les Datacenters 💬")
+    
+    # Load the document
+    docx = 'monographie.docx'
+    
+    if docx is not None:
+        # Lire le texte du document
+        text = docx2txt.process(docx)
+
+        # Afficher toujours la barre de saisie
+        st.markdown('<div class="input-space"></div>', unsafe_allow_html=True)
+        selected_questions = st.sidebar.radio("****Choisir :****", questions)
+        # Afficher toujours la barre de saisie
+        query_input = st.text_input("", key="text_input_query", placeholder="Posez votre question ici...", help="Posez votre question ici...")
+        st.markdown('<div class="input-space"></div>', unsafe_allow_html=True)
+
         if query_input and query_input not in st.session_state.previous_question:
             query = query_input
             st.session_state.previous_question.append(query_input)
         elif selected_questions:
             query = selected_questions
         else:
-            query=""
-    
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
- 
-            llm = OpenAI(model="gpt-3.5-turbo-instruct")
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                if "Donnez-moi un résumé du rapport" in query:
-                    response = "Le rapport Monographie sectorielle détaille le secteur des Datacenters au Maroc et dans le monde, en mettant en lumière le marché en pleine croissance, les technologies clés, et les stratégies de développement à l'échelle nationale et internationale. Il aborde l'essor du marché marocain des Datacenters, soulignant son potentiel de croissance grâce à la stratégie nationale du numérique et l'ambition du Maroc de devenir un hub technologique africain. Le document explore également les récents développements du secteur à l'échelle mondiale, y compris les tendances, les technologies de pointe, et l'importance des Datacenters dans la transformation digitale. Des analyses spécifiques sur l'infrastructure, les services offerts, et les défis tels que la sécurité des données et l'efficacité énergétique sont inclus, ainsi que des perspectives sur les opportunités d'investissement et les implications pour le futur du secteur."
+            query = ""
 
-                conversation_history.add_user_message(query)
-                conversation_history.add_ai_message(response)  # Utilisez add_ai_message
-        
-            formatted_messages = []
+        if query :
+            st.session_state.conversation_history.add_user_message(query)  # Ajouter à l'historique
 
-            for msg in conversation_history.messages:
-                role = "user" if msg.type == "human" else "assistant"
-                avatar = "🧑" if role == "user" else "🤖"
-                css_class = "user-message" if role == "user" else "assistant-message"
         
-                message_div = f'<div class="{css_class}">{msg.content}</div>'
-                avatar_div = f'<div class="avatar">{avatar}</div>'
-        
-                if role == "user":
-                    formatted_message = f'<div class="message-container user"><div class="message-avatar">{avatar_div}</div><div class="message-content">{message_div}</div></div>'
-                else:
-                    formatted_message = f'<div class="message-container assistant"><div class="message-content">{message_div}</div><div class="message-avatar">{avatar_div}</div></div>'
-        
-                formatted_messages.append(formatted_message)
+            messages = [
+                {
+                    "role": "user",
+                    "content": (
+                        f"{query}. En tenant compte du texte suivant essayer de ne pas dire le texte ne contient pas les informations si ne trouve pas à partir de texte répondre d'aprés votre connaissance stp et ne dire pas stp le texte incomplet ou incompréhensible essayer de formulé une bon réponse sans critiquer le texte par exemple ne pas dire texte fragmenter ou quelque chose comme ça répondre directement stp parceque je vais l'afficher o lecteur: {text} "
+                    )
+                }
+            ]
 
-            messages_html = "\n".join(formatted_messages)
-            st.markdown(messages_html, unsafe_allow_html=True)
+            # Appeler l'API OpenAI pour obtenir le résumé
+            response = openai.ChatCompletion.create(
+                model="gpt-4o-mini",
+                messages=messages
+            )
 
-            # Affichage des avatars à l'extérieur de la div des messages
+            # Récupérer le contenu de la réponse
+
+            summary = response['choices'][0]['message']['content']
+            if "Donnez-moi un résumé du rapport" in query:
+                summary = "Le rapport Monographie sectorielle détaille le secteur des Datacenters au Maroc et dans le monde, en mettant en lumière le marché en pleine croissance, les technologies clés, et les stratégies de développement à l'échelle nationale et internationale. Il aborde l'essor du marché marocain des Datacenters, soulignant son potentiel de croissance grâce à la stratégie nationale du numérique et l'ambition du Maroc de devenir un hub technologique africain. Le document explore également les récents développements du secteur à l'échelle mondiale, y compris les tendances, les technologies de pointe, et l'importance des Datacenters dans la transformation digitale. Des analyses spécifiques sur l'infrastructure, les services offerts, et les défis tels que la sécurité des données et l'efficacité énergétique sont inclus, ainsi que des perspectives sur les opportunités d'investissement et les implications pour le futur du secteur."
+
+                # Votre logique pour traiter les réponses
+            #conversation_history.add_user_message(query)
+            #conversation_history.add_ai_message(response)
+            st.session_state.conversation_history.add_ai_message(summary)  # Ajouter à l'historique
             
+            # Afficher la question et le résumé de l'assistant
+            #conversation_history.add_user_message(query)
+            #conversation_history.add_ai_message(summary)
+
+            # Format et afficher les messages comme précédemment
+            formatted_messages = []
+            previous_role = None 
+            if st.session_state.conversation_history.messages: # Variable pour stocker le rôle du message précédent
+                    for msg in conversation_history.messages:
+                        role = "user" if msg.type == "human" else "assistant"
+                        avatar = "🧑" if role == "user" else "🤖"
+                        css_class = "user-message" if role == "user" else "assistant-message"
+
+                        if role == "user" and previous_role == "assistant":
+                            message_div = f'<div class="{css_class}" style="margin-top: 25px;">{msg.content}</div>'
+                        else:
+                            message_div = f'<div class="{css_class}">{msg.content}</div>'
+
+                        avatar_div = f'<div class="avatar">{avatar}</div>'
+                
+                        if role == "user":
+                            formatted_message = f'<div class="message-container user"><div class="message-avatar">{avatar_div}</div><div class="message-content">{message_div}</div></div>'
+                        else:
+                            formatted_message = f'<div class="message-container assistant"><div class="message-content">{message_div}</div><div class="message-avatar">{avatar_div}</div></div>'
+                
+                        formatted_messages.append(formatted_message)
+                        previous_role = role  # Mettre à jour le rôle du message précédent
+
+                    messages_html = "\n".join(formatted_messages)
+                    st.markdown(messages_html, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
